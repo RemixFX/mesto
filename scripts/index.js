6821dbf176b29076d23bc8af25548ca48cfbb946
Remix <remix.master@yandex.ru>
2021-08-20 20:14:04 +0300

    fix: header/popup/js

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,20 @@
 let popupMenuButton = document.querySelector('.profile__edit-button');
-let popupMenu = document.querySelector('.popup');
-let popupCloseButton = document.querySelector('.popup__close-button');
-let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__input_type_name');
-let jobInput = document.querySelector('.popup__input_type_about-me');
+let popupMenu =  document.querySelector('.popup');
+let popupCloseButton =  popupMenu.querySelector('.popup__close-button');
+let formElement =  popupMenu.querySelector('.form');
+let nameInput =  popupMenu.querySelector('.form__input_type_name');
+let jobInput =  popupMenu.querySelector('.form__input_type_job');
 let nameOutput = document.querySelector('.profile__name');
-let jobOutput = document.querySelector('.profile__about-me');
+let jobOutput = document.querySelector('.profile__job');
 
 
 function toggleForm(evt) {
   popupMenu.classList.toggle('popup_opened');
   evt.preventDefault();
+  if (popupMenu.classList.contains('popup_opened')) {
+    nameInput.value = nameOutput.textContent;
+    jobInput.value = jobOutput.textContent;
+  }
 };
 
 function formSubmitHandler(evt) {
@@ -20,11 +24,6 @@ function formSubmitHandler(evt) {
   toggleForm(evt);
 };
 
-popupCloseButton.addEventListener('click', function() {
-  formElement.reset();
-});
-
 popupMenuButton.addEventListener('click', toggleForm);
 popupCloseButton.addEventListener('click', toggleForm);
 formElement.addEventListener('submit', formSubmitHandler);
-
