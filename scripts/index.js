eb87eed49045d54a25b8f4a0bd9e1aed5f1dbecd
Remix <remix.master@yandex.ru>
2021-08-27 19:16:30 +0300

    feat: добавить функцию добавления карточек через popup

--- scripts/index.js
+++ scripts/index.js
@@ -27,3 +27,87 @@ function formSubmitHandler(evt) {
 popupMenuButton.addEventListener('click', toggleForm);
 popupCloseButton.addEventListener('click', toggleForm);
 formElement.addEventListener('submit', formSubmitHandler);
+
+//1. Шесть карточек «из коробки»
+
+const elementsContainer = document.querySelector('.elements');
+const elementsTemplate = document.querySelector('#elements-template').content;
+//const linkCard = document.querySelector('.element__image').src;
+//const nameCard = document.querySelector('.element__name').textContent;
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    alt: 'горные хребты с переходом от заснеженных верхушек до зеленых склонов'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+    alt: 'ещё не замерзшее озере на опушке леса в зимнюю пору'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+    alt: 'плотная застройка однотипными девятиэтажными домами'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+    alt: 'темный грунт, редкая растительность, высокая гора с заснеженным склоном'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+    alt: 'железная дорога уходящая в даль, вдоль лесистой местности'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+    alt: 'зимнее озеро с причудливо застывшими корками льда'
+  }
+];
+
+// единая функция добавления карточек
+
+const AddCard = (element) => {
+  const card = elementsTemplate.querySelector('.element').cloneNode(true);
+
+  card.querySelector('.element__image').src = element.link;
+  card.querySelector('.element__image').alt = element.alt;
+  card.querySelector('.element__name').textContent = element.name;
+  elementsContainer.prepend(card);
+};
+
+
+initialCards.forEach(element => AddCard(element));
+
+//2. Форма добавления карточки
+
+const popupMenuAddCard =  document.querySelector('.popup_type_add-card');
+const AddCardButton = document.querySelector('.profile__add-button');
+const AddCardCloseButton = popupMenuAddCard.querySelector('.popup__close-button_type_add-card');
+
+function toggleFormAddCard() {
+  popupMenuAddCard.classList.toggle('popup_opened');
+};
+
+AddCardButton.addEventListener('click', toggleFormAddCard);
+AddCardCloseButton.addEventListener('click', toggleFormAddCard);
+
+//3. Добавление карточки
+
+const formAddCard =  popupMenuAddCard.querySelector('.form');
+const nameCardInput = popupMenuAddCard.querySelector('.form__input_type_name-card');
+const linkCardInput = popupMenuAddCard.querySelector('.form__input_type_link');
+
+function addCardSubmitHandler(evt) {
+  evt.preventDefault();
+  AddCard({
+    link: linkCardInput.value,
+    name: nameCardInput.value
+  });
+  formAddCard.reset();
+  toggleFormAddCard();
+};
+
+formAddCard.addEventListener('submit', addCardSubmitHandler);
